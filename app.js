dd3e6ed0c52f4aa77c5473f25f677e6bfd7463ca
rwhelm <rwhelm@example.com>
2021-07-28 14:45:23 -0400

    Full pipeline test.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('DevOps Rules'));
+app.get('/', (req, res) => res.send('DevOps Rules 2'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
